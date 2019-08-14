5306316a9f081a5046c18df01f2f43f24c14c6f3
Volodymyr <31970034+datsikvv@users.noreply.github.com>
2019-08-15 00:09:15 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+var app =require('express')();
+var http =require('http').Server(app);
+var io =require('socket.io')(http);
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+var users = [];
+io.on('connection', function(socket) {
+	console.log('A user connected');
+	socket.on('setUserName', function(data) {
+		console.log(data);
+		if (users.indexOf(data) > -1) {
+			console.log(data);
+			socket.emit('userExists', '<p class="bg-primary"> Пользователь ' + 
+									  '<b>' + data + '</b>' + 
+									  ' уже существует, выбери другое имя!</p>');
+		} else {
+			users.push(data);
+			socket.emit('userSet', {userName: data});
+		}
+	});
+
+	socket.on('message', function (data) {
+		io.sockets.emit('newMessage', data);
+	})
+});
+
+http.listen(3000, '192.168.1.2' || 'localhost', function() {
+	console.log('Go to chat at http://localhost:3000');
+})
